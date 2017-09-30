4647f29ee41eb57c65907319c5684207c5632cbd
Abhi-Sony-PC <iabhisharma12@gmail.com>
2017-09-30 09:16:59 +0530

    Added Values

--- Assignment1.js
+++ Assignment1.js
@@ -8,7 +8,7 @@ var restify = require('restify'),
 
 patientSave = require('save')('patients'),
 
-server = restify.createServer({name:patient-api})
+server = restify.createServer({name:'patient-api'})
 
 server.listen(port,host,function(){
     console.log('Server %s listening at %s',server.name,server.url)
