566ce53234a320763242c10037c62ab38ee2c96d
Abhi-Sony-PC <iabhisharma12@gmail.com>
2017-09-30 09:08:25 +0530

    Storing data

--- Assignment1.js
+++ Assignment1.js
@@ -1,1 +1,18 @@
-var serv
+var server_name = 'patient-api'
+
+var port = '8000'
+
+var host = '127.0.0.1'
+
+var restify = require('restify'),
+
+patientSave = require('save')('patients'),
+
+server = restify.createServer({name:patient-api})
+
+server.listen(port,host,function(){
+    console.log('Server %s listening at %s',server.name,server.url)
+    console.log('Resources:')
+    console.log('/patients')
+    console.log('/patients/:id')
+})
